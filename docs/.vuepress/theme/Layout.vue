a389d82db50f1460fee703c8915fba07352f033c
borsha06 <tasfia.sharmin1994@gmail.com>
2018-11-05 21:36:01 +0600

    initial

---
+++ docs/.vuepress/theme/Layout.vue
@@ -0,0 +1,33 @@
+<template>
+<div class="theme-container">
+    <component class="main-content" :is="layout"/>
+</div>
+</template>
+<script>
+    import Vue from 'vue'
+    import Va from '../../../node_modules/vue-atlas/src/index.js'
+    Vue.use(Va, 'en')
+    import home from './layouts/home.vue';
+    import post from './layouts/post.vue';
+
+
+    export default {
+            components: {
+                'home': home
+            },
+        computed: {
+            layout(){
+                const { path } = this.$page;
+                if(path === '/')
+                {
+                    return 'home'
+                }
+                else
+                {
+                    return 'post'
+                }
+            }
+        }
+
+    }
+</script>
---
+++ docs/.vuepress/theme/layouts/post.vue
@@ -0,0 +1,9 @@
+<template><va-layout-manager>
+    <va-page :size="size" :article="article">
+        <article>
+           <content custom="false"></content>
+        </article>
+        Welcome
+    </va-page>
+</va-layout-manager>
+</template>
